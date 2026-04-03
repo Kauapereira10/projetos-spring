ab660ee87e4d5cbc4a10003928a20b0d3ff919c2
Kauã Pereira <borgeskaua1210@gmail.com>
2026-04-02 23:37:35 -0300

    feat: adiciona DTO para operações de depósito e saque
    Cria a classe DepositaryAndWithdrawRequest para representar os dados de entrada
    nas operações de depósito e saque.
    
    - Contém o campo value para valor da transação
    - Utiliza Lombok (@Getter e @Setter) para reduzir boilerplate
    - Usado nos endpoints de depósito e saque no AccountController

---
+++ bank-api/src/main/java/com/kaua/bank/bank_api/dto/DepositaryAndWithdrawRequest.java
@@ -0,0 +1,12 @@
+package com.kaua.bank.bank_api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class DepositaryAndWithdrawRequest {
+
+    private double value;
+
+}
