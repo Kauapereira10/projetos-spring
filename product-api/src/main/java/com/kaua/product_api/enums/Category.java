a0f0b8555edb2ac3a55a7febe84b9d1198bf7e34
Kauã Pereira <borgeskaua1210@gmail.com>
2026-04-20 16:52:16 -0300

    feat: adiciona enum Category com categorias de produtos
    Define as categorias disponíveis para classificação de um produto.

---
+++ product-api/src/main/java/com/kaua/product_api/enums/Category.java
@@ -0,0 +1,14 @@
+package com.kaua.product_api.enums;
+
+public enum Category {
+
+    ELECTRONICS,
+    BOOKS,
+    FOOD,
+    CLOTHING,
+    HOME,
+    BEAUTY,
+    SPORTS,
+    TOYS,
+    AUTOMOTIVE
+}
